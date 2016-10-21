1efbe9120a5be09da0459c66e1ba546aefa82ce3
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-10-21 18:10:01 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+
+from setuptools import setup, find_packages
+
+setup(name='aiostream',
+      packages=find_packages(),
+      setup_requires=['pytest_runner'],
+      tests_require=['pytest', 'pytest_asyncio'],
+      )
