72fdecd680cb7e01aa57438803f79cb03073233c
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-10-21 18:13:32 +0200

    Add coverage

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 setup(name='aiostream',
       packages=find_packages(),
       setup_requires=['pytest_runner'],
-      tests_require=['pytest', 'pytest_asyncio'],
+      tests_require=['pytest', 'pytest-asyncio', 'pytest-cov'],
       )
