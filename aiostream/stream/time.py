bfcda7089d20b9078e140874aa4787fc5bba76b9
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-10-24 20:04:09 +0200

    Add timeout operator

--- aiostream/stream/time.py
+++ aiostream/stream/time.py
@@ -1,11 +1,12 @@
 
 import asyncio
 
+from ..utils import anext
 from ..core import operator
 
 
 @operator(pipable=True)
-async def space_out(stream, interval):
+async def space_out(source, interval):
     timeout = 0
     loop = asyncio.get_event_loop()
     async with source.stream() as streamer:
@@ -13,3 +14,16 @@ async def space_out(stream, interval):
             await asyncio.sleep(timeout - loop.time())
             yield item
             timeout = loop.time() + interval
+
+
+@operator(pipable=True)
+async def timeout(source, timeout):
+    loop = asyncio.get_event_loop()
+    async with source.stream() as streamer:
+        while True:
+            try:
+                item = await asyncio.wait_for(anext(streamer), timeout)
+            except StopAsyncIteration:
+                break
+            else:
+                yield item
