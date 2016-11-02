6ba2c0f319fc4a1c90d9394282facb8a8d11bbf9
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-11-02 18:15:47 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup
 
-setup(name='aiostream',
-      packages=find_packages(),
-      setup_requires=['pytest_runner'],
-      tests_require=['pytest', 'pytest-asyncio', 'pytest-cov'],
-      )
+if sys.version_info < (3, 6):
+    raise RuntimeError("aiostream requires Python 3.6")
+
+TESTING = any(x in sys.argv for x in ['test', 'pytest'])
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name='aiostream',
+    version='0.1.0',
+
+    packages=['aiostream', 'aiostream.stream'],
+    setup_requires=['pytest-runner' if TESTING else ''],
+    tests_require=['pytest', 'pytest-asyncio', 'pytest-cov'],
+
+    description="Generator-based operators for asynchronous iteration",
+    long_description=long_description,
+    url="https://pypi.python.org/pypi/aiostream",
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+    ],
+
+    author="Vincent Michel",
+    author_email="vxgmichel@gmail.com",
+)
