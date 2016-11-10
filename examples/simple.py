d252e35def019f8cc7bc23852775d3980d300fb7
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-11-10 11:19:38 +0100

    Update simple example

--- examples/simple.py
+++ examples/simple.py
@@ -1,24 +1,21 @@
 import asyncio
 from aiostream import stream, pipe
 
-# This stream computes 11² + 13² in 1.5 second
-xs = (
-    stream.count(interval=0.1)         # Count from zero every 0.1 s
-    | pipe.skip(10)                    # Skip the first 10 numbers
-    | pipe.take(5)                     # Take the following 5
-    | pipe.filter(lambda x: x % 2)     # Keep odd numbers
-    | pipe.map(lambda x: x ** 2)       # Square the results
-    | pipe.reduce(lambda x, y: x + y)  # Add the numbers together
-)
 
-# The stream can be awaited
+async def main():
+    # This stream computes 11² + 13² in 1.5 second
+    xs = (
+        stream.count(interval=0.1)      # Count from zero every 0.1 s
+        | pipe.skip(10)                 # Skip the first 10 numbers
+        | pipe.take(5)                  # Take the following 5
+        | pipe.filter(lambda x: x % 2)  # Keep odd numbers
+        | pipe.map(lambda x: x ** 2)    # Square the results
+        | pipe.accumulate()             # Add the numbers together
+    )
+    print('11² + 13² = ', await xs)
+
+
+# Run main coroutine
 loop = asyncio.get_event_loop()
-result = loop.run_until_complete(xs)
-print('11² + 13² = ', result)
-
-# The stream can run several times
-result = loop.run_until_complete(xs)
-print('11² + 13² = ', result)
-
-# Clean up
+loop.run_until_complete(main())
 loop.close()
