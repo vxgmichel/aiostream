1bf6e0c6a2bbc6e83281b12610deb483c1caf156
Vincent Michel <vincent.michel@esrf.fr>
2018-01-03 17:19:57 +0100

    Update map operator to be concurrent

--- aiostream/stream/combine.py
+++ aiostream/stream/combine.py
@@ -53,29 +53,78 @@ async def zip(*sources):
 
 
 @operator(pipable=True)
-async def map(source, func, *more_sources):
+async def smap(source, func, *more_sources):
+    """Apply a given function to the elements of one or several
+    asynchronous sequences.
+
+    Each element is used as a positional argument, using the same order as
+    their respective sources. The generation continues until the shortest
+    sequence is exhausted. The function is treated synchronously.
+
+    Note: if more than one sequence is provided, they're awaited concurrently
+    so that their waiting times don't add up.
+    """
+    if more_sources:
+        source = zip(source, *more_sources)
+    async with streamcontext(source) as streamer:
+        async for item in streamer:
+            yield func(*item) if more_sources else func(item)
+
+
+@operator(pipable=True)
+def amap(source, corofn, *more_sources, ordered=True, task_limit=None):
+    """Apply a given coroutine function to the elements of one or several
+    asynchronous sequences.
+
+    Each element is used as a positional argument, using the same order as
+    their respective sources. The generation continues until the shortest
+    sequence is exhausted.
+
+    The results can either be returned in or out of order, depending on
+    the corresponding ordered argument.
+
+    The coroutines run concurrently but their amount can be limited using
+    the task_limit argument. A value of 1 will cause the coroutines to run
+    sequentially.
+
+    Note: if more than one sequence is provided, they're also awaited
+    concurrently, so that their waiting times don't add up.
+    """
+    op = lambda *args: create.just(corofn(*args))
+    if ordered:
+        return advanced.concatmap.raw(
+            source, op, *more_sources, task_limit=task_limit)
+    return advanced.flatmap.raw(
+        source, op, *more_sources, task_limit=task_limit)
+
+
+@operator(pipable=True)
+def map(source, func, *more_sources, ordered=True, task_limit=None):
     """Apply a given function to the elements of one or several
     asynchronous sequences.
 
     Each element is used as a positional argument, using the same order as
     their respective sources. The generation continues until the shortest
     sequence is exhausted. The function can either be synchronous or
-    asynchronous.
+    asynchronous (coroutine function).
 
-    Note: the different sequences are awaited in parallel, so that their
-    waiting times don't add up.
+    The results can either be returned in or out of order, depending on
+    the corresponding ordered argument. This argument is ignored if the
+    provided function is synchronous.
+
+    The coroutines run concurrently but their amount can be limited using
+    the task_limit argument. A value of 1 will cause the coroutines to run
+    sequentially. This argument is ignored if the provided function is
+    synchronous.
+
+    Note: if more than one sequence is provided, they're also awaited
+    concurrently, so that their waiting times don't add up.
     """
-    iscorofunc = asyncio.iscoroutinefunction(func)
-    if more_sources:
-        source = zip(source, *more_sources)
-    async with streamcontext(source) as streamer:
-        async for item in streamer:
-            if not more_sources:
-                item = (item,)
-            result = func(*item)
-            if iscorofunc:
-                result = await result
-            yield result
+    if asyncio.iscoroutinefunction(func):
+        return amap.raw(
+            source, func, *more_sources,
+            ordered=ordered, task_limit=task_limit)
+    return smap.raw(source, func, *more_sources)
 
 
 @operator(pipable=True)
