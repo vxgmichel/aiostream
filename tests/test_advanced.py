da62512824edf7dd309200fe23b8c4650d5c7260
Vincent Michel <vincent.michel@esrf.fr>
2018-01-04 12:46:05 +0100

    Update tests for advanced operators

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -9,30 +9,78 @@ assert_run, event_loop
 
 @pytest.mark.asyncio
 async def test_concatmap(assert_run, event_loop):
+    # Concurrent run
     with event_loop.assert_cleanup():
         xs = stream.range(0, 6, 2, interval=1)
         ys = xs | pipe.concatmap(lambda x: stream.range(x, x+2, interval=5))
         await assert_run(ys, [0, 1, 2, 3, 4, 5])
         assert event_loop.steps == [1, 1, 3, 1, 1]
 
+    # Sequential run
+    with event_loop.assert_cleanup():
+        xs = stream.range(0, 6, 2, interval=1)
+        ys = xs | pipe.concatmap(
+            lambda x: stream.range(x, x+2, interval=5),
+            task_limit=1)
+        await assert_run(ys, [0, 1, 2, 3, 4, 5])
+        assert event_loop.steps == [5, 1, 5, 1, 5]
+
+    # Limited run
+    with event_loop.assert_cleanup():
+        xs = stream.range(0, 6, 2, interval=1)
+        ys = xs | pipe.concatmap(
+            lambda x: stream.range(x, x+2, interval=5),
+            task_limit=2)
+        await assert_run(ys, [0, 1, 2, 3, 4, 5])
+        assert event_loop.steps == [1, 4, 1, 5]
+
+    # Make sure item arrive as soon as possible
+    with event_loop.assert_cleanup():
+        xs = stream.just(2)
+        ys = xs | pipe.concatmap(lambda x: stream.range(x, x+4, interval=1))
+        zs = ys | pipe.timeout(2)  # Sould NOT raise
+        await assert_run(zs, [2, 3, 4, 5])
+        assert event_loop.steps == [1, 1, 1]
+
 
 @pytest.mark.asyncio
 async def test_flatmap(assert_run, event_loop):
+    # Concurrent run
     with event_loop.assert_cleanup():
-        xs = stream.range(0, 3, interval=1)
-        ys = xs | pipe.flatmap(lambda x: stream.range(x, x+6, 3, interval=3))
+        xs = stream.range(0, 6, 2, interval=1)
+        ys = xs | pipe.flatmap(lambda x: stream.range(x, x+2, interval=5))
+        await assert_run(ys, [0, 2, 4, 1, 3, 5])
+        assert event_loop.steps == [1, 1, 3, 1, 1]
+
+    # Sequential run
+    with event_loop.assert_cleanup():
+        xs = stream.range(0, 6, 2, interval=1)
+        ys = xs | pipe.flatmap(
+            lambda x: stream.range(x, x+2, interval=5),
+            task_limit=1)
         await assert_run(ys, [0, 1, 2, 3, 4, 5])
-        assert event_loop.steps == [1, 1, 1, 1, 1]
+        assert event_loop.steps == [5, 1, 5, 1, 5]
+
+    # Limited run
+    with event_loop.assert_cleanup():
+        xs = stream.range(0, 6, 2, interval=1)
+        ys = xs | pipe.flatmap(
+            lambda x: stream.range(x, x+2, interval=5),
+            task_limit=2)
+        await assert_run(ys, [0, 2, 1, 3, 4, 5])
+        assert event_loop.steps == [1, 4, 1, 5]
 
 
 @pytest.mark.asyncio
 async def test_switchmap(assert_run, event_loop):
-    with event_loop.assert_cleanup():
-        xs = stream.range(0, 5, interval=1)
-        ys = xs | pipe.switchmap(lambda x: stream.range(x, x+2, interval=2))
-        await assert_run(ys, [0, 1, 2, 3, 4, 5])
-        assert event_loop.steps == [1, 1, 1, 1, 1, 1]
 
+    with event_loop.assert_cleanup():
+        xs = stream.range(0, 30, 10, interval=3)
+        ys = xs | pipe.switchmap(lambda x: stream.range(x, x+5, interval=1))
+        await assert_run(ys, [0, 1, 2, 10, 11, 12, 20, 21, 22, 23, 24])
+        assert event_loop.steps == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    # Test cleanup procedure
     with event_loop.assert_cleanup():
         xs = stream.range(0, 5, interval=1)
         ys = xs | pipe.switchmap(lambda x: stream.range(x, x+2, interval=2))
