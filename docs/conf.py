d12d15e06f5c3ce18d7652140c06408f2028a178
Vincent Michel <vincent.michel@esrf.fr>
2018-05-14 13:21:36 +0200

    Fix syntax highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ author = 'Vincent Michel'
 copyright = u'2016, Vincent Michel'
 
 master_doc = 'index'
-highlight_language = 'python3'
+highlight_language = 'python'
 extensions = ['sphinx.ext.autodoc']
 
 html_theme = "sphinx_rtd_theme"
