de0bdabfe3a4c320300f4e85982b5277243581f2
Vincent Michel <vincent.michel@esrf.fr>
2018-07-25 13:23:33 +0200

    Add python 3.7 support

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 
     author="Vincent Michel",
