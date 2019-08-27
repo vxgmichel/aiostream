2d99ff7cba10b6057db8f395fad7c6d0f9f0db1e
Vincent Michel <vxgmichel@gmail.com>
2019-08-27 18:46:57 +0200

    Bump version 0.3.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.rst", encoding="utf-8") as f:
 
 setup(
     name='aiostream',
-    version='0.3.3.dev0',
+    version='0.3.3',
 
     packages=['aiostream', 'aiostream.stream'],
     setup_requires=['pytest-runner' if TESTING else ''],
