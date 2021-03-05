c55091064d86ca6f5e9b191b30a1f975fd3ffda1
Vincent Michel <vxgmichel@gmail.com>
2021-03-05 12:06:29 +0100

    Update python classifiers

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     author="Vincent Michel",
     author_email="vxgmichel@gmail.com",
